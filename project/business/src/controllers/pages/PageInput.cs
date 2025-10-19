d32b61a46d3a81cd5b602f6dcefc4266e1978e1b
afbc-04 <andrefbc2004@gmail.com>
2025-10-19 10:35:20 +0100

    FEAT: Multiple order arguments
    - Added possibility of ordering with multiple arguments
    - Changed order format

--- project/business/src/controllers/pages/PageInput.cs
+++ project/business/src/controllers/pages/PageInput.cs
@@ -5,14 +5,27 @@ namespace Pages {
         public long page {get; private set;}
         public long limit {get; private set;}
         public string? sort {get; private set;}
-        public bool? order {get; private set;}
 
-        public PageInput(long page, long limit, string? sort, bool? order) {
+        public PageInput(long page, long limit, List<(string,bool)> sort_args) {
             
             this.page = page;
             this.limit = limit;
-            this.sort = sort;
-            this.order = order;
+            this.sort = null;
+
+            if (sort_args.Count() > 0) {
+
+                string sorting_sql = "ORDER BY ";
+                List<string> sorting_args = new();
+
+                foreach ((string arg, bool is_asc) in sort_args) {
+                    string asc = is_asc == true ? "ASC" : "DESC";
+                    sorting_args.Add($"{arg} {asc}");
+                }
+                
+                sorting_sql += string.Join(", ",sorting_args);
+                this.sort = sorting_sql;
+                
+            }
 
         }
 
@@ -20,16 +33,8 @@ namespace Pages {
 
             string sql = "";
 
-            if (this.sort != null) {
-
-                sql += $" ORDER BY {this.sort}";
-
-                if (this.order != null) {
-                    string order = this.order == true ? "ASC" : "DESC";
-                    sql += $" {order}";
-                }
-
-            }
+            if (this.sort != null)
+                sql += $" {this.sort}";
 
             sql += $" LIMIT {this.limit} OFFSET {(this.page-1) * this.limit}";
 
--- project/business/src/controllers/pages/PageRequest.cs
+++ project/business/src/controllers/pages/PageRequest.cs
@@ -1,4 +1,5 @@
 using PacketHandlers;
+using System.Text.RegularExpressions;
 
 namespace Pages {
 
@@ -9,16 +10,15 @@ namespace Pages {
         public string? page {get; private set;}
         public string? limit {get; private set;}
         public string? sort {get; private set;}
-        public string? order {get; private set;}
+        public List<(string,bool)> sort_list {get; private set;}
 
         public PageRequest(HttpRequest request) {
 
             this.sort  = PacketUtils.get_value_from_query(request, "sort")?.Trim();
-            this.order = PacketUtils.get_value_from_query(request, "order")?.Trim();
             this.page  = PacketUtils.get_value_from_query(request, "page")?.Trim();
             this.limit = PacketUtils.get_value_from_query(request, "limit")?.Trim();
+            this.sort_list = new();
 
-            Console.WriteLine($"P {this.page} L {this.limit} S {this.sort} O {this.order}");
         }
 
         public IList<string> validate(IDictionary<string,string> sort_possibilities) {
@@ -49,18 +49,27 @@ namespace Pages {
 
             if (this.sort != null) {
 
-                if (sort_possibilities.Keys.Contains(this.sort!.ToLower()) == false) {
-                    string sort_opts = string.Join(", ",sort_possibilities.Keys);
-                    error_list.Add($"Sort argument is not valid. Try these ones : [{sort_opts}]");
-                }
+                if (Regex.IsMatch(this.sort,@"(\w[\w_]*:-?1)(,(\w[\w_]*:-?1))*") == false)
+                    error_list.Add($"Sort arguments are not valid. List should have the format : [<name>:(1 or -1)] (item divided by ',')");
                 else {
-                    this.sort = sort_possibilities[this.sort];
+
+                    string[] sorting_tokens = this.sort.Split(",");
+                    string sort_opts = string.Join(", ",sort_possibilities.Keys);
+
+                    foreach (string token in sorting_tokens) {
+
+                        string[] token_args = token.Split(":");
+                        bool is_asc = Convert.ToInt32(token_args[1]) == 1;
+
+                        if (sort_possibilities.Keys.Contains(token_args[0].ToLower()) == false) 
+                            error_list.Add($"Sort argument {token_args[0]} is not valid. Try these ones : [{sort_opts}]");
+                        else 
+                            this.sort_list.Add((sort_possibilities[token_args[0]],is_asc));
+
+                    }
+
                 }
 
-            }
-
-            if (this.order != null && PageRequest.order_possibilities.Contains(this.order!.ToLower()) == false) {
-                error_list.Add($"""Order argument is not valid. Try these ones : ["asc", "desc"]""");
             }
 
             return error_list;
@@ -71,22 +80,14 @@ namespace Pages {
 
             long page_page = 1;
             long page_limit = 10;
-            bool? page_order = null;
 
             if (this.page != null)
                 page_page = Convert.ToInt64(this.page);
 
             if (this.limit != null)
                 page_limit = Convert.ToInt64(this.limit);
-            
-            if (this.order != null)
-                page_order = order.ToUpper() switch {
-                    "ASC" => true,
-                    "DESC" => false,
-                    _ => null
-                };
 
-            return new PageInput(page_page,page_limit,this.sort,page_order);
+            return new PageInput(page_page,page_limit,this.sort_list);
 
         }
 
